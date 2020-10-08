b4c6578fc47c3631cbca47cd70dfc6cacf8a3073
Jan Bruedigam <jan.bruedigam@t-online.de>
2020-10-08 18:28:38 +0200

    Suboptimal solution

--- examples/elliptic_path.jl
+++ examples/elliptic_path.jl
@@ -28,7 +28,7 @@ link1 = Body(box)
     end
       
     vb=params(qb)[2:4]                                   
-    eqc1=SA[vb[1]-f();vb[2];vb[3]]
+    eqc1=SA[abs(vb[1])-abs(f());vb[2];vb[3]]
     eqc2=SA[xb[1]-xa[1]; ((xb[2]-xa[2])^2/a^2)+((xb[3]-xa[3])^2/b^2)-1]
     G= [eqc1 ; eqc2]
     return G
@@ -39,15 +39,11 @@ end
     b=2
     function f()
         if (xb[2]==0) 
-            xb[3]<0 ? θ=-pi/4 : θ=pi/4
+            xb[3]>0 ? θ = pi : θ = 0
         else
-            m1=(xb[3]*a^2)/(xb[2]*b^2)                                 
-            α=atan(m1)                        
-            if xb[3]<0                        
-                xb[2]*xb[3]<0 ? θ=(pi/2)+α : θ=-(pi/2)+α 
-            else
-                xb[2]*xb[3]<0 ? θ=-(pi/2)+α : θ=(pi/2)+α            
-            end       
+            m1=(xb[3]*a^2)/(xb[2]*b^2)                                 #Slope of the normal
+            α=atan(m1)                                                 #Angle between normal and the horizontal axis 
+            xb[2]>0 ? θ=(pi/2)+α : θ=-(pi/2)+α                   #Angle between normal and the vertical axis 
         end
         return sin(θ/2)
     end
@@ -67,10 +63,10 @@ constraints = [joint_between_origin_and_link1]
 shapes = [box]
 
 
-mech = Mechanism(origin, links, constraints, shapes = shapes,g=-9.81)
-setPosition!(link1,x=[0;-5.0;1.0],q = UnitQuaternion(RotX(-pi/2)))
+mech = Mechanism(origin, links, constraints, shapes = shapes)
+setPosition!(link1,x=[0;0.1;2],q = UnitQuaternion(RotX(pi+0.1)))
 
-initializeConstraints!(mech,newtonIter = 100)
+initializeConstraints!(mech,newtonIter = 400)
 
 steps = Base.OneTo(1000)
 storage = Storage{Float64}(steps,1)
