3ef96ef1107fcb475b80c89f624fa04589a8b913
Jan Bruedigam <jan.bruedigam@t-online.de>
2020-10-09 11:49:58 +0200

    Fix elliptic path

--- examples/elliptic_path.jl
+++ examples/elliptic_path.jl
@@ -37,18 +37,29 @@ end
 @inline function g(joint::my_constraint, xb::AbstractVector, qb::UnitQuaternion)
     a=5
     b=2
-    function f()
-        if (xb[2]==0) 
-            xb[3]>0 ? θ = pi : θ = 0
-        else
-            m1=(xb[3]*a^2)/(xb[2]*b^2)                                 #Slope of the normal
-            α=atan(m1)                                                 #Angle between normal and the horizontal axis 
-            xb[2]>0 ? θ=(pi/2)+α : θ=-(pi/2)+α                   #Angle between normal and the vertical axis 
-        end
-        return sin(θ/2)
+    # function f()
+    #     if (xb[2]==0) 
+    #         xb[3]>0 ? θ = pi : θ = 0
+    #     else
+    #         m1=(xb[3]*a^2)/(xb[2]*b^2)                                 #Slope of the normal
+    #         α=atan(m1)                                                 #Angle between normal and the horizontal axis 
+    #         xb[2]>0 ? θ=(pi/2)+α : θ=-(pi/2)+α                   #Angle between normal and the vertical axis 
+    #     end
+    #     return sin(θ/2)
+    # end
+
+    if xb[3]==0
+        xb[2] > 0 ? α = -pi/2 : α = pi/2
+    elseif xb[3] > 0
+        α = -atan(b^2/a^2*xb[2]/xb[3])
+    else
+        α = -atan(b^2/a^2*xb[2]/xb[3]) - pi
     end
-    vb=params(qb)[2:4]
-    eqc1=SA[vb[1]-f();vb[2];vb[3]]
+
+    qα = UnitQuaternion(cos(α/2),sin(α/2),0,0,false)
+
+    # vb=params(qb)[2:4]
+    eqc1=Vmat(qb\qα)
     eqc2=SA[xb[1]; (xb[2]^2/a^2)+(xb[3]^2/b^2)-1]
     G= [eqc1;eqc2]
     return G
@@ -63,8 +74,9 @@ constraints = [joint_between_origin_and_link1]
 shapes = [box]
 
 
-mech = Mechanism(origin, links, constraints, shapes = shapes)
-setPosition!(link1,x=[0;0.1;2],q = UnitQuaternion(RotX(pi+0.1)))
+mech = Mechanism(origin, links, constraints, shapes = shapes, g=-10)
+setPosition!(link1,x=[0;0.01;2],q = UnitQuaternion(RotX(0)))
+setVelocity!(link1,v=[0;2.0;0])
 
 initializeConstraints!(mech,newtonIter = 400)
 
